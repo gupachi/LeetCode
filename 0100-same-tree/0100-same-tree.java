031ee90e00ccaea0bff00e45ad399e1b2d53ca59
jiwon02 <86184712+gupachi@users.noreply.github.com>
2023-08-03 14:18:51 +0900

    Time: 0 ms (100.00%), Space: 40 MB (67.44%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,31 @@
+
+// Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+          this.left = left;
+         this.right = right;
+      }
+  }
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+    if((p==null) && (q==null)){
+        return true;
+    }
+    if((p!= null) && (q != null)){
+        if(p.val == q.val){
+          boolean t = isSameTree(p.left,q.left);
+          boolean f = isSameTree(p.right,q.right);
+          return (t&&f);
+    }
+    
+    }
+     return false;
+    
+}}
